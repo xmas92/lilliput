20947b69ea1e3ee60ba6043622fcddd89d8c1f56
Axel Boldt-Christmas <axel.boldt-christmas@oracle.com>
2024-04-15 11:46:42 +0000

    8315884: New Object to ObjectMonitor mapping

--- src/hotspot/share/runtime/globals.hpp
+++ src/hotspot/share/runtime/globals.hpp
@@ -1967,6 +1967,27 @@ const int ObjectAlignmentInBytes = 8;
           "2: monitors & new lightweight locking (LM_LIGHTWEIGHT, default)") \
           range(0, 2)                                                       \
                                                                             \
+  product(bool, UseObjectMonitorTable, false, DIAGNOSTIC,                   \
+          "With Lightweight Locking mode, use a table to record inflated "  \
+          "monitors rather than the first word of the object.")             \
+                                                                            \
+  product(bool, OMC2UnrollCacheLookupLoopTail, true, "")                    \
+                                                                            \
+  product(int, OMC2UnrollCacheEntries, 0, "")                               \
+          range(0, OMCache::CAPACITY)                                       \
+                                                                            \
+  product(int, OMCacheSize, 8, "")                                          \
+          range(0, OMCache::CAPACITY)                                       \
+                                                                            \
+  product(int, OMSpins, 13,                                                 \
+          "Specifies the number of time lightweight fast locking will "     \
+          "attempt to CAS the markWord before inflating. Between each "     \
+          "CAS it will spin for exponentially more time, resulting in "     \
+          "a total number of spins on the order of O(2^OMSpins)")           \
+          range(1, 30)                                                      \
+                                                                            \
+  product(bool, OMCacheHitRate, false, "")                                  \
+                                                                            \
   product(uint, TrimNativeHeapInterval, 0,                                  \
           "Interval, in ms, at which the JVM will trim the native heap if " \
           "the platform supports that. Lower values will reclaim memory "   \
